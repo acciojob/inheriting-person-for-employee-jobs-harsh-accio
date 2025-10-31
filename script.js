84a3cbf628561910c8fc52cb6744422d69d69f3b
harsh-accio <rajputharsh9897@gmail.com>
2025-10-31 08:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ class Employee extends Person{
 		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`
 	}
 }
+// Creating an instance of Person
+const person = new Person("Alice", 25);
+person.greet();
+// Expected Output: Hello, my name is Alice, I am 25 years old.
+
+// Creating an instance of Employee
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
+// Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
 
 
 
